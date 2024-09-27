16fad0c76a78a41d292bea76eecedab5e84c89e8
Enzowax <144791348+Enzowax@users.noreply.github.com>
2024-09-27 10:22:22 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include<windows.h>
 #include<string>
 using namespace std;
@@ -6,25 +6,29 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int choice;
+    int v;
     cout << "Выберите задачу:\n";
     cout << "1. Проверить квадрат или прямоугольник\n";
     cout << "2. Проверить тип треугольника\n";
     cout << "Введите номер: ";
-    cin >> choice;
+    cin >> v;
 
-    if (choice == 1) {
+    if (v == 1) {
         int a, b;
-        cout << "Введите длину первой стороны: ";
+        cout << "длина первой стороны: ";
         cin >> a;
-        cout << "Введите длину второй стороны: ";
+        cout << "длина второй стороны: ";
         cin >> b;
         cout << (a == b ? "Это квадрат.\n" : "Это прямоугольник.\n");
     }
-    else if (choice == 2) {
+    else if (v == 2) {
         int a, b, c;
-        cout << "Введите длины трех сторон треугольника: ";
-        cin >> a >> b >> c;
+        cout << "длина первой стороны: ";
+        cin >> a;
+        cout << "длина второй стороны: ";
+        cin >> b;
+        cout << "длина третий стороны: ";
+        cin >> c;
         cout << (a == b && b == c ? "Треугольник равносторонний.\n" :
             (a == b || b == c || a == c ? "Треугольник равнобедренный.\n" :
                 "Треугольник разносторонний.\n"));
